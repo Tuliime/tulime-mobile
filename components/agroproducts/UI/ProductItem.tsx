e66f7724f1ac9f90c3c734faa231eea50a0a6da9
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-01-27 14:27:17 +0300

    refactor: add shodow to product item card

--- components/agroproducts/UI/ProductItem.tsx
+++ components/agroproducts/UI/ProductItem.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 import { View, Image, Text } from "react-native";
-import { TProduct } from "@/types/product";
+import { TAgroproducts } from "@/types/product";
 import { SIZES, COLORS } from "@/constants";
 import { truncateString, addCommasToNumber } from "@/utils";
 
-export const ProductItem: React.FC<TProduct & { itemWidth: number }> = (
-  props
-) => {
+export const ProductItem: React.FC<
+  TAgroproducts["product"] & {
+    itemWidth: number;
+    price: number;
+    currency: string;
+  }
+> = (props) => {
   return (
     <View
       style={{
         width: props.itemWidth,
         backgroundColor: COLORS.white,
         borderRadius: 8,
+        elevation: 5,
       }}
     >
       <Image
-        source={{ uri: `${props.image}` }}
+        source={{ uri: `${props.imageUrl}` }}
         resizeMode="contain"
         style={{
           width: "100%",
@@ -26,8 +31,10 @@ export const ProductItem: React.FC<TProduct & { itemWidth: number }> = (
         }}
       />
       <View style={{ padding: SIZES.small, paddingTop: 0 }}>
-        <Text>{truncateString(props.name)}</Text>
-        <Text style={{ color: COLORS.primary, fontWeight: 500 }}>
+        <Text style={{ color: COLORS.gray8 }}>
+          {truncateString(props.name)}
+        </Text>
+        <Text style={{ color: COLORS.primary, fontWeight: 600 }}>
           {`${props.currency} ${addCommasToNumber(props.price)}`}
         </Text>
       </View>

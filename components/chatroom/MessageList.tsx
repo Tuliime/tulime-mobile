b82e28ec7c3cfd232ff48fb193f7976ca7a3b9bf
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-02-26 15:39:36 +0300

    feat: add auto scroll bottom on sending message

--- components/chatroom/MessageList.tsx
+++ components/chatroom/MessageList.tsx
@@ -1,4 +1,10 @@
-import React, { useCallback, useMemo, useRef, useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { FlatList, View, StyleSheet, Text } from "react-native";
 import { SenderMessage } from "./SenderMessage";
 import { TChatroom } from "@/types/chatroom";
@@ -13,11 +19,17 @@ import { router } from "expo-router";
 
 export const ChatroomMessageList: React.FC = () => {
   const flatListRef = useRef<FlatList>(null);
+  const [isAtTop, setIsAtTop] = useState(true);
   const currentUser = useAuthStore((state) => state.auth.user);
   const users = useAuthStore((state) => state.users) ?? [];
   const messages = useChatroomStore((state) => state.messages) ?? [];
   const replies = useChatroomStore((state) => state.replies) ?? [];
-  const postingMessage = useChatroomStore((state) => state.postingMessage);
+  const getAllPostingMessages = useChatroomStore(
+    (state) => state.getAllPostingMessages
+  );
+  const postingMessageMap = useChatroomStore(
+    (state) => state.postingMessageMap
+  );
   const [currentMessageDate, setCurrentMessageDate] = useState<string | null>(
     null
   );
@@ -72,13 +84,33 @@ export const ChatroomMessageList: React.FC = () => {
   );
 
   // Scroll to bottom on first render and when posting a message
-  // useEffect(() => {
-  //   if (flatListRef.current) {
-  //     setTimeout(() => {
-  //       flatListRef.current?.scrollToEnd({ animated: true });
-  //     }, 100);
-  //   }
-  // }, [postingMessage.status]);
+  useEffect(() => {
+    if (flatListRef.current && isAtTop) {
+      setTimeout(() => {
+        flatListRef.current?.scrollToOffset({ offset: 20, animated: true });
+      }, 100);
+    }
+  }, []);
+
+  useEffect(() => {
+    const scrollToBottomOnPostingMessage = () => {
+      console.log("Scrolling to the bottom...");
+      const now = Date.now();
+      const postingMessagePending: boolean = !!getAllPostingMessages().find(
+        (postingMessage) =>
+          postingMessage.status === "pending" &&
+          now - 3000 <= new Date(postingMessage.sentAt).getTime()
+      );
+      console.log("postingMessagePending: ", postingMessagePending);
+      if (flatListRef.current && postingMessagePending) {
+        console.log("Scrolled to the bottom...");
+        setTimeout(() => {
+          flatListRef.current?.scrollToEnd({ animated: true });
+        }, 100);
+      }
+    };
+    scrollToBottomOnPostingMessage();
+  }, [postingMessageMap]);
 
   const onScrollStartHandler = () => {
     setShowDateOnScroll(() => true);
@@ -90,8 +122,13 @@ export const ChatroomMessageList: React.FC = () => {
 
   const onScrollHandler = (event: any) => {
     const { contentOffset, layoutMeasurement, contentSize } = event.nativeEvent;
+    setIsAtTop(() => contentOffset.y === 0);
 
-    if (contentOffset.y <= 0) {
+    const isFlatListTop: boolean = contentOffset.y <= 0;
+    const isFlatListBottom: boolean =
+      contentOffset.y + layoutMeasurement.height >= contentSize.height;
+
+    if (isFlatListTop) {
       console.log("Reached the top");
       router.setParams({
         cursor: organizedMessages[0].id,
@@ -101,8 +138,9 @@ export const ChatroomMessageList: React.FC = () => {
       });
     }
 
-    if (contentOffset.y + layoutMeasurement.height >= contentSize.height) {
+    if (isFlatListBottom) {
       console.log("Reached the bottom");
+      if (isFlatListBottom) return;
       router.setParams({
         cursor: organizedMessages[organizedMessages.length - 1].id,
         includeCursor: "false",

c054b87c6fe8f4f288610caeb8b930658c3a92d6
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-02-27 14:19:36 +0300

    feat: add sound on message post and new message arrival

---
+++ constants/sounds.ts
@@ -0,0 +1,7 @@
+const levelUpSound = require("@/assets/sounds/level-up.mp3");
+const tapNotificationSound = require("@/assets/sounds/tap-notification.mp3");
+
+export default {
+  levelUpSound,
+  tapNotificationSound,
+};

d4969abd62adee9e9db113103f8be7413717c817
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-03-14 19:34:30 +0300

    feat: add functionality to post new device

---
+++ hooks/useGetDeviceToken.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { getExpoPushToken } from "@/utils/getExpoPushToken";
+import { useDeviceStore } from "@/store/device";
+import { useMutation } from "@tanstack/react-query";
+import { device } from "@/API/device";
+import { useAuthStore } from "@/store/auth";
+// import DeviceInfo from "react-native-device-info";
+import * as Device from "expo-device";
+
+export const useGetDeviceToken = () => {
+  const currentDevice = useDeviceStore((state) => state.currentDevice);
+  const updateCurrentDevice = useDeviceStore(
+    (state) => state.updateCurrentDevice
+  );
+  const accessToken = useAuthStore((state) => state.auth.accessToken);
+  const userID = useAuthStore((state) => state.auth.user.id);
+  const [deviceName, setDeviceName] = useState("");
+
+  const { isPending, mutate } = useMutation({
+    mutationFn: device.post,
+    onSuccess: (response: any) => {
+      console.log("device posted successfully:", response);
+      updateCurrentDevice(response.data);
+    },
+    onError: (error) => {
+      console.log("Error posting device token:", error);
+    },
+  });
+
+  useEffect(() => {
+    const postDeviceToken = async () => {
+      if (!!currentDevice.token) return;
+      const deviceToken = await getExpoPushToken()!;
+
+      //   DeviceInfo.getDeviceName().then((name) => {
+      //     console.log("Device name: ", name);
+      //     setDeviceName(() => name);
+      //   });
+
+      if (Device.deviceName) {
+        setDeviceName(() => Device.deviceName!);
+        console.log("deviceName: ", deviceName);
+      }
+      if (!deviceToken || !deviceName) return;
+
+      mutate({
+        userID: userID,
+        deviceToken: deviceToken,
+        name: deviceName,
+        tokenType: "EXPO",
+        accessToken: accessToken,
+      });
+    };
+    postDeviceToken();
+  }, [currentDevice, deviceName]);
+};

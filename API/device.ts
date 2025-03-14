ebf3af91701ad756c40d5a310cd296d2ad4804cb
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-03-14 19:34:30 +0300

    feat: add functionality to post new device

---
+++ API/device.ts
@@ -0,0 +1,55 @@
+import { serverURL } from "@/constants/urls";
+
+class DeviceAPI {
+  post = async ({
+    name,
+    userID,
+    deviceToken,
+    tokenType,
+    accessToken,
+  }: {
+    name: string;
+    userID: string;
+    deviceToken: string;
+    tokenType: string;
+    accessToken: string;
+  }) => {
+    const response = await fetch(`${serverURL}/device/`, {
+      method: "POST",
+      body: JSON.stringify({
+        name: name,
+        userID: userID,
+        token: deviceToken,
+        tokenType: tokenType,
+      }),
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      throw new Error(error.message);
+    }
+    return await response.json();
+  };
+
+  getByUser = async ({ userID, token }: { userID: string; token: string }) => {
+    const response = await fetch(`${serverURL}/device/user/${userID}`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      throw new Error(error.message);
+    }
+    return await response.json();
+  };
+}
+
+export const device = new DeviceAPI();
---
+++ store/device.ts
@@ -0,0 +1,94 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { produce, enableMapSet } from "immer";
+import { TDevice } from "@/types/device";
+import { TStoreHydration } from "@/types/store";
+
+enableMapSet(); // Enable Map & Set support for Immer
+
+const initialDevice = {
+  id: "",
+  userID: "",
+  token: "",
+  tokenType: "",
+  name: "",
+  notificationDisabled: false,
+  createdAt: "",
+  updatedAt: "",
+};
+
+type TUseDeviceStore = {
+  currentDevice: TDevice["device"];
+  devices: TDevice["device"][]; //All user devices
+} & TDevice["deviceAction"] &
+  TStoreHydration;
+
+export const useDeviceStore = create(
+  persist<TUseDeviceStore>(
+    (set) => ({
+      currentDevice: initialDevice,
+      devices: [],
+
+      // Device Actions
+      updateCurrentDevice: (device) =>
+        set(
+          produce((state: TUseDeviceStore) => {
+            state.currentDevice = device;
+          })
+        ),
+      updateAllDevices: (devices) =>
+        set(
+          produce((state: TUseDeviceStore) => {
+            state.devices = devices;
+          })
+        ),
+      AddDevice: (newDevice) =>
+        set(
+          produce((state: TUseDeviceStore) => {
+            const alreadyHasDevice: boolean = !!state.devices.find(
+              (device: TDevice["device"]) => device.id === newDevice.id
+            );
+            if (alreadyHasDevice) return;
+            state.devices.push(newDevice);
+          })
+        ),
+      clearAllDevices: () =>
+        set(
+          produce((state: TUseDeviceStore) => {
+            state.devices = [];
+            state.currentDevice = initialDevice;
+          })
+        ),
+
+      // Track Hydration State
+      _hasHydrated: false,
+      setHasHydrated: (state: any) => {
+        set({
+          _hasHydrated: state,
+        });
+      },
+    }),
+    {
+      name: "device-storage",
+      storage: {
+        getItem: async (key) => {
+          const value = await AsyncStorage.getItem(key);
+          return value ? JSON.parse(value) : null;
+        },
+        setItem: async (key, value) => {
+          await AsyncStorage.setItem(key, JSON.stringify(value));
+        },
+        removeItem: async (key) => {
+          await AsyncStorage.removeItem(key);
+        },
+      },
+      onRehydrateStorage: (state) => {
+        return () => {
+          state.setHasHydrated(true);
+          console.log("useDeviceStore store hydrated");
+        };
+      },
+    }
+  )
+);

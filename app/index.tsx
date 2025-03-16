d0b783e0cde7e58fbc66da7dff9da44533b5e75d
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-03-16 12:33:07 +0300

    fix(auth): wait for 'signInWithRT' fn to resolve before redirecting to any screen

--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,9 @@ import { useAuthStore } from "@/store/auth";
 import { useSignInWithRefreshToken } from "@/hooks/useSignInWithRefreshToken";
 import { isJWTTokenExpired } from "@/utils/expiredJWT";
 
+// TODO: To display the UI when running this page
 export default function Index() {
-  const { auth, updateAuth, deleteAuth } = useAuthStore();
+  const { auth, deleteAuth } = useAuthStore();
   const { accessToken, refreshToken } = auth;
   const [redirectPath, setRedirectPath] = useState<string | null>(null);
   const { signInWithRT } = useSignInWithRefreshToken();
@@ -15,39 +16,47 @@ export default function Index() {
 
   // TODO: to introduce 3 hour check
   useEffect(() => {
-    if (!accessToken) {
-      console.log("No accessToken found, redirecting to signin.");
-      deleteAuth();
-      setRedirectPath("/auth/signin");
-      return;
-    }
-    if (!isExpiredAccessToken) {
-      setRedirectPath(() => "/home");
-      console.log("AccessToken is valid, redirecting to home.");
-      return;
-    }
-
-    if (isExpiredAccessToken) {
-      if (!isExpiredRefreshToken) {
-        const successfulSignin = signInWithRT({
-          userID: auth.user.id,
-          refreshToken: refreshToken,
-        });
-        if (!successfulSignin) {
-          console.log("Sign in with RT failed, redirecting to signin.");
-          setRedirectPath(() => "/auth/signin");
-          return;
-        }
-        console.log("Expired accessToken, redirecting to signin.");
-        setRedirectPath(() => "/home");
+    const authCheckHandler = async () => {
+      if (!accessToken) {
+        console.log("No accessToken found, redirecting to signin.");
+        deleteAuth();
+        setRedirectPath("/auth/signin");
         return;
       }
 
-      console.log("Expired accessToken and refresh, redirecting to signin.");
-      setRedirectPath(() => "/auth/signin");
-      return;
-    }
-  }, [accessToken, refreshToken, deleteAuth, updateAuth]);
+      if (!isExpiredAccessToken) {
+        console.log("AccessToken is valid, redirecting to home.");
+        setRedirectPath("/home");
+        return;
+      }
+
+      if (isExpiredAccessToken && !isExpiredRefreshToken) {
+        const successfulSignin = await signInWithRT({
+          userID: auth.user.id,
+          refreshToken: refreshToken,
+        });
+
+        if (!successfulSignin) {
+          console.log("Sign in with RT failed, redirecting to signin.");
+          setRedirectPath("/auth/signin");
+          return;
+        }
+
+        console.log(
+          "Expired accessToken, successfully refreshed. Redirecting to home."
+        );
+        setRedirectPath("/home");
+        return;
+      }
+
+      console.log(
+        "Expired accessToken and refresh token, redirecting to signin."
+      );
+      setRedirectPath("/auth/signin");
+    };
+
+    authCheckHandler();
+  }, [accessToken, refreshToken, deleteAuth]);
 
   console.log("Invoked the index screen.");
 

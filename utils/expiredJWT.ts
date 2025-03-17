16ed601fd5b06c94df466a9dd48ecd550384f14a
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-03-17 15:50:43 +0300

    clean: remove unnecessary comments

--- utils/expiredJWT.ts
+++ utils/expiredJWT.ts
@@ -3,7 +3,6 @@ import { jwtDecode, JwtPayload } from "jwt-decode";
 export const isJWTTokenExpired = (token: string): boolean => {
   try {
     const decoded = jwtDecode<JwtPayload>(token);
-    console.log("Decoded Token:", decoded);
     if (decoded.exp) {
       return decoded.exp * 1000 < Date.now();
     }

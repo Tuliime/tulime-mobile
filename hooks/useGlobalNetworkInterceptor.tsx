206c3d9f3a97cac2ab76842f25c71578971eb5fb
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-03-17 15:26:36 +0300

    feat: add global request interceptor to for request retry functionality

---
+++ hooks/useGlobalNetworkInterceptor.tsx
@@ -0,0 +1,56 @@
+import { useAuthStore } from "@/store/auth";
+import { useEffect } from "react";
+import { View } from "react-native";
+import { useSignInWithRefreshToken } from "./useSignInWithRefreshToken";
+import { router } from "expo-router";
+
+export const UseGlobalRequestInterceptor = () => {
+  const { auth, deleteAuth } = useAuthStore();
+  const { refreshToken } = auth;
+  const { signInWithRT } = useSignInWithRefreshToken();
+
+  useEffect(() => {
+    const fetchInterceptorHandler = () => {
+      const originalFetch = global.fetch;
+
+      global.fetch = async (...args) => {
+        let response = await originalFetch(...args);
+
+        console.log(`API Call: ${args[0]} | Status Code: ${response.status}`);
+
+        if (response.status === 403) {
+          // Auto signin user with RT
+          const signIn = await signInWithRT({
+            userID: auth.user.id,
+            refreshToken: refreshToken,
+          });
+
+          // Retry the request
+          if (signIn.isSuccess) {
+            const headers: any = args[1]?.headers;
+            headers.Authorization = `Bearer ${signIn.response?.accessToken}`;
+            args[1]!["headers"] = headers;
+
+            response = await originalFetch(...args);
+            console.log(
+              `API Call: ${args[0]} | Status Code: ${response.status}`
+            );
+          }
+
+          if (!signIn.isSuccess) {
+            console.log("Auto sign in with RT failed, redirecting to signin.");
+            deleteAuth();
+            router.push("/auth/signin");
+
+            await new Promise((resolve) => setTimeout(resolve, 2000));
+          }
+        }
+
+        return response;
+      };
+    };
+    fetchInterceptorHandler();
+  }, []);
+
+  return <View></View>;
+};

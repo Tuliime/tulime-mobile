e0c51e931bdf943614b6705ba8b8f573a024c018
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-04-12 22:20:24 +0300

    feat: add store api functions

---
+++ API/store.ts
@@ -0,0 +1,146 @@
+import { serverURL } from "@/constants/urls";
+
+class StoreAPI {
+  post = async ({
+    userID,
+    name,
+    description,
+  }: {
+    userID: string;
+    name: string;
+    description: string;
+  }) => {
+    const response = await fetch(`${serverURL}/store`, {
+      method: "POST",
+      body: JSON.stringify({
+        userID: userID,
+        name: name,
+        description: description,
+      }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      throw new Error(error.message);
+    }
+    return await response.json();
+  };
+
+  update = async ({
+    storeID,
+    userID,
+    name,
+    description,
+    website,
+    email,
+    location,
+  }: {
+    storeID: string;
+    userID: string;
+    name: string;
+    description: string;
+    website: string;
+    email: string;
+    location: string;
+  }) => {
+    const response = await fetch(`${serverURL}/store/${storeID}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        userID: userID,
+        name: name,
+        description: description,
+        website: website,
+        email: email,
+        location: location,
+      }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      throw new Error(error.message);
+    }
+    return await response.json();
+  };
+
+  updateBgImage = async ({
+    storeID,
+    formData,
+  }: {
+    formData: FormData;
+    storeID: string;
+  }) => {
+    const response = await fetch(`${serverURL}/store/${storeID}/bg-image`, {
+      method: "PATCH",
+      body: formData,
+      headers: {
+        "Content-type": "multipart/form-data",
+      },
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      throw new Error(error.message);
+    }
+    return await response.json();
+  };
+
+  updateLogo = async ({
+    storeID,
+    formData,
+  }: {
+    formData: FormData;
+    storeID: string;
+  }) => {
+    const response = await fetch(`${serverURL}/store/${storeID}/logo`, {
+      method: "PATCH",
+      body: formData,
+      headers: {
+        "Content-type": "multipart/form-data",
+      },
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      throw new Error(error.message);
+    }
+    return await response.json();
+  };
+
+  getAll = async () => {
+    const response = await fetch(`${serverURL}/store`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      throw new Error(error.message);
+    }
+    return await response.json();
+  };
+
+  getByUser = async ({ userID }: { userID: string }) => {
+    const response = await fetch(`${serverURL}/store/user/${userID}`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      throw new Error(error.message);
+    }
+    return await response.json();
+  };
+}
+
+export const store = new StoreAPI();

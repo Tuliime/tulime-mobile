b5d7b3ee77d359da5fc1f1e01c51b90c5b776280
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-04-21 17:01:17 +0300

    feat: add functionality to show preview of remote images in the image previewer

--- components/shared/UI/ImagePreview.tsx
+++ components/shared/UI/ImagePreview.tsx
@@ -12,37 +12,68 @@ import {
 
 type ImagePreviewProps = {
   files: Asset["file"][];
+  urls?: string[];
   onDelete: (index: number) => void;
+  canDelete?: boolean;
+  isLocalImage?: boolean;
 };
 
 export const ImagePreview: React.FC<ImagePreviewProps> = ({
   files,
   onDelete,
+  canDelete,
+  isLocalImage,
+  urls,
 }) => {
+  const showDeleteIcon = canDelete !== undefined ? canDelete : true;
+  const isLocal = isLocalImage !== undefined ? isLocalImage : true;
+
   return (
     <ScrollView
       horizontal
       showsHorizontalScrollIndicator={false}
       style={styles.container}
     >
-      {files.map((file, index) => {
-        return (
-          <View key={index} style={styles.imageContainer}>
-            <TouchableOpacity
-              style={styles.deleteImageBtn}
-              onPress={() => onDelete(index)}
-            >
-              <AntDesign name="close" size={14} color={COLORS.gray7} />
-            </TouchableOpacity>
-            <Image
-              key={file.name}
-              resizeMode="cover"
-              source={{ uri: `data:${file.mimeType};base64,${file.base64}` }}
-              style={styles.image}
-            />
-          </View>
-        );
-      })}
+      {isLocal && (
+        <>
+          {files.map((file, index) => {
+            return (
+              <View key={index} style={styles.imageContainer}>
+                {showDeleteIcon && (
+                  <TouchableOpacity
+                    style={styles.deleteImageBtn}
+                    onPress={() => onDelete(index)}
+                  >
+                    <AntDesign name="close" size={14} color={COLORS.gray7} />
+                  </TouchableOpacity>
+                )}
+                <Image
+                  resizeMode="cover"
+                  source={{
+                    uri: `data:${file.mimeType};base64,${file.base64}`,
+                  }}
+                  style={styles.image}
+                />
+              </View>
+            );
+          })}
+        </>
+      )}
+      {!isLocal && (
+        <>
+          {urls!.map((url, index) => {
+            return (
+              <View key={index} style={styles.imageContainer}>
+                <Image
+                  resizeMode="cover"
+                  source={{ uri: url }}
+                  style={styles.image}
+                />
+              </View>
+            );
+          })}
+        </>
+      )}
     </ScrollView>
   );
 };
